176fe21f7267d022727a313f8c715f6bac7a05c6
FlorianHarchut <flha.mbox@gmail.com>
2021-06-23 13:06:47 +0200

    Überarbeitete Version Unklarheiten besprochen Fertig Befüllt

--- Hangman.java
+++ Hangman.java
@@ -1,49 +1,66 @@
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import java.io.IOException;
-
 public class Hangman {
-    
-    private static Woerterbuch woerterbuch;
-    private static Benutzeroberflaeche benutzeroberflaeche;
-    private static Spielregeln spielregeln;
-    
-    private static int anzahlbuchstaben;
-    private static String zufallswort;
-    private static char buchstabe;
-    private static int fehlerzaehler;
-    
-    public static void main(String[] args) {
-
-        woerterbuch = new Woerterbuch();
-        benutzeroberflaeche = new Benutzeroberflaeche();
-        spielregeln = new Spielregeln();
-
-        spielregeln.doSpielZuruecksetzen();
-        benutzeroberflaeche.doInitialisieren();
-        
-        anzahlbuchstaben = benutzeroberflaeche.getAnzahlBuchstaben(); 
-        zufallswort = woerterbuch.getZufallswort(anzahlbuchstaben);
-        spielregeln.setSpielStarten(zufallswort);
-
-        do {
-            benutzeroberflaeche.doWortAnzeigen(spielregeln.getAnzeigewort());
-            buchstabe = benutzeroberflaeche.getBuchstabeRaten();
-            if (spielregeln.isBuchstabeRichtig(buchstabe)) {
-                benutzeroberflaeche.doEingabeRichtig();
-            } else {
-                benutzeroberflaeche.doEingabeFalsch();
-            }
-            fehlerzaehler = spielregeln.getFehlerzaehler();
-            benutzeroberflaeche.doGalgenAnzeigen(fehlerzaehler);
-        }
-        while (spielregeln.isSpielBeendet()==false);
-        
-        benutzeroberflaeche.doWortAnzeigen(zufallswort);
-        if (spielregeln.isSpielVerloren()) {
-            benutzeroberflaeche.doSpielVerloren();
-        } else {
-            benutzeroberflaeche.doSpielGewonnen();
+// interne Unterklassen für Funktionalität:
+private static Woerterbuch woerterbuch; // zufälliges Wort ermitteln
+private static Spielregeln spielregeln; // Regeln und Spielstand
+private static Benutzeroberflaeche benutzeroberflaeche; // Ein- und Ausgaben
+// interne Variablen für Daten:
+private static int anzahlbuchstaben; // Buchstaben des zu ratenden Wortes
+private static String zufallswort; // gesuchtes zufälliges Wort im Klartext
+private static char buchstabe; // eingegebener geratener Buchstabe
+private static int fehlerzaehler; // Anzahl falsch geratener Buchstaben
+public static void main(String[] args)
+throws Exception {
+// Objekte erzeugen
+woerterbuch = new Woerterbuch();
+spielregeln = new Spielregeln();
+benutzeroberflaeche = new BenutzeroberflaecheKonsole();
+// Spiel initialisieren
+spielregeln.doSpielZuruecksetzen();
+benutzeroberflaeche.doInitialisieren();
+// Zufallswort ermitteln (solange wiederholen, bis kein Eingabefehler)
+zufallswort = null;
+while (zufallswort == null) {
+try { // Fehler Benutzeroberfläche+Wörterbuch abfangen
+anzahlbuchstaben = benutzeroberflaeche.getAnzahlBuchstaben();
+zufallswort = woerterbuch.getZufallswort(anzahlbuchstaben);
+}
+catch (Exception e) {
+benutzeroberflaeche.doFehlermeldung(e.toString());
+}
+}
+spielregeln.setSpielStarten(zufallswort);
+// Buchstaben raten (solange wiederholen, bis Spielende)
+benutzeroberflaeche.doAusgabeLoeschen();
+benutzeroberflaeche.doGalgenAnzeigen(0);
+do {
+// Wort mit Platzhaltern anzeigen und Buchstabe eingeben
+benutzeroberflaeche.doWortAnzeigen(spielregeln.getAnzeigewort());
+buchstabe = 0;
+while (buchstabe == 0) {
+try { // Fehler von Benutzeroberfläche abfangen
+buchstabe = benutzeroberflaeche.getBuchstabeRaten();
+}
+catch (Exception e) {
+benutzeroberflaeche.doFehlermeldung(e.toString());
+}
+}
+// Buchstabe prüfen und Galgen ausgeben
+benutzeroberflaeche.doAusgabeLoeschen();
+if (spielregeln.isBuchstabeRichtig(buchstabe)) {
+benutzeroberflaeche.doEingabeRichtig();
+} else {
+benutzeroberflaeche.doEingabeFalsch();
+}
+fehlerzaehler = spielregeln.getFehlerzaehler();
+benutzeroberflaeche.doGalgenAnzeigen(fehlerzaehler);
+}
+while (spielregeln.isSpielBeendet()==false);
+// Spielende
+benutzeroberflaeche.doWortAnzeigen(zufallswort);
+if (spielregeln.isSpielVerloren()) {
+benutzeroberflaeche.doSpielVerloren();
+} else {
+benutzeroberflaeche.doSpielGewonnen();
         }
 
     }
--- Woerterbuch.java
+++ Woerterbuch.java
@@ -1,13 +1,118 @@
-
-
+// Spiel Hangman – Woerterbuch - Gruppenarbeit Florian, Manuel, Maxi, Jacob
+import java.util.Random;
 public class Woerterbuch {
-
-    public String getZufallswort(int buchstaben) {
-        return "bus";
-    }
-
-    public String getZufallswort() {
-        return getZufallswort(3);
-    }        
-        
+// interne Konstanten für minimal/maximale Anzahl
+private static final int MIN_BUCHSTABEN = 2; // minimale Anzahl Buchstaben
+private static final int MAX_BUCHSTABEN = 3; // maximale Anzahl Buchstaben
+private int getZufallszahl(int min, int max) {
+// Zufallszahl im Bereich [min,max] generieren.
+// Zufallsgenerator abhängig von der Uhrzeit initialisieren.
+Random zufallsgenerator = new Random();
+zufallsgenerator.setSeed(System.currentTimeMillis());
+int zufallszahl = zufallsgenerator.nextInt((max - min) + 1) + min;
+return zufallszahl;
 }
+public String getZufallswort(int buchstaben)
+throws Exception {
+// Zufallswort mit Anzahl [buchstaben] generieren.
+// Fehler, wenn kein Wort gefunden werden konnte.
+// https://www.wort-suchen.de/wortlisten/woerter-mit-2-buchstaben
+String zufallswort = null;
+int zufallszahl = getZufallszahl(1,9);
+switch(buchstaben) {
+case 0:
+case 1:
+return getZufallswort();
+case 2:
+switch(zufallszahl) {
+case 1: zufallswort = "ab"; break;
+case 2: zufallswort = "du"; break;
+case 3: zufallswort = "in"; break;
+case 4: zufallswort = "ja"; break;
+case 5: zufallswort = "ob"; break;
+case 6: zufallswort = "so"; break;
+case 7: zufallswort = "um"; break;
+case 8: zufallswort = "wo"; break;
+default: zufallswort = "zu"; break;
+}
+break;
+case 3:
+switch(zufallszahl) {
+case 1: zufallswort = "Axt"; break;
+case 2: zufallswort = "Bus"; break;
+case 3: zufallswort = "Dom"; break;
+case 4: zufallswort = "Eis"; break;
+case 5: zufallswort = "Fan"; break;
+case 6: zufallswort = "Gut"; break;
+case 7: zufallswort = "Job"; break;
+case 8: zufallswort = "Klo"; break;
+default: zufallswort = "Lok"; break;
+}
+case 4:
+switch(zufallszahl) {
+case 1: zufallswort = "Bahn"; break;
+case 2: zufallswort = "Auto"; break;
+case 3: zufallswort = "Haus"; break;
+case 4: zufallswort = "Maus"; break;
+case 5: zufallswort = "Hand"; break;
+case 6: zufallswort = "Lohn"; break;
+case 7: zufallswort = "Kiwi"; break;
+case 8: zufallswort = "Nein"; break;
+default: zufallswort = "Kopf"; break;
+}
+case 5:
+switch(zufallszahl) {
+case 1: zufallswort = "Tante"; break;
+case 2: zufallswort = "Onkel"; break;
+case 3: zufallswort = "Manga"; break;
+case 4: zufallswort = "Filme"; break;
+case 5: zufallswort = "Spiel"; break;
+case 6: zufallswort = "Handy"; break;
+case 7: zufallswort = "Wagen"; break;
+case 8: zufallswort = "Salat"; break;
+default: zufallswort = "Zange"; break;
+}
+
+case 6:
+switch(zufallszahl) {
+case 1: zufallswort = "Cabrio"; break;
+case 2: zufallswort = "Backen"; break;
+case 3: zufallswort = "hacker"; break;
+case 4: zufallswort = "brillen"; break;
+case 5: zufallswort = "kaktus"; break;
+case 6: zufallswort = "Barren"; break;
+case 7: zufallswort = "Ausruf"; break;
+case 8: zufallswort = "Aufzug"; break;
+default: zufallswort = "Banane"; break;
+}
+case 7:
+switch(zufallszahl) {
+case 1: zufallswort = "puzzels"; break;
+case 2: zufallswort = "komisch"; break;
+case 3: zufallswort = "tablett"; break;
+case 4: zufallswort = "absturz"; break;
+case 5: zufallswort = "beamter"; break;
+case 6: zufallswort = "sportler"; break;
+case 7: zufallswort = "boxclub"; break;
+case 8: zufallswort = "polizei"; break;
+default: zufallswort = "eckball"; break;
+}
+break;
+default:
+throw new Exception(
+
+"Kein Wort gefunden ("+MIN_BUCHSTABEN+".."
++MAX_BUCHSTABEN+" Buchstaben)!");
+
+}
+return zufallswort;
+}
+public String getZufallswort()
+throws Exception {
+// Zufallswort mit zufälliger Anzahl Buchstaben generieren.
+// Fehler, wenn kein Wort gefunden werden konnte.
+int zufallszahl = getZufallszahl(MIN_BUCHSTABEN,MAX_BUCHSTABEN);
+return getZufallswort(zufallszahl);
+}
+}
+
